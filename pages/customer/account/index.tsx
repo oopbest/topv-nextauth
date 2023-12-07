2af7253bd629ab40b362cddbacf98c8e9f07e7fa
Setthawut Pimthong <oop_best@yahoo.com>
2023-12-07 16:50:26 +0700

    add account page

---
+++ pages/customer/account/index.tsx
@@ -0,0 +1,20 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
+type Props = {};
+
+export default function MyProfile() {
+  const { data: session } = useSession();
+  return (
+    <div>
+      <h1>Welcome to my profile</h1>
+      {session && session.user ? (
+        <>
+          <h5>{session.user.email}</h5>
+          <button onClick={() => signOut()}>Logout</button>
+        </>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
