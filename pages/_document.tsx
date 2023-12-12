f21e41d2f4c4e562263f4c1e896f8752d914844e
Setthawut Pimthong <oop_best@yahoo.com>
2023-12-12 12:14:15 +0700

    test script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -8,6 +8,33 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+
+      <>
+        <div id="fb-root"></div>
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: ` chatbox = document.getElementById('fb-customer-chat');
+                chatbox.setAttribute("page_id", "397245167074344");
+                chatbox.setAttribute("attribution", "biz_inbox");
+                </script>
+                <script>
+                window.fbAsyncInit = function() {
+                FB.init({
+                xfbml            : true,
+                version          : 'v11.0'
+                });
+                };
+                (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));`,
+          }}
+        ></script>
+      </>
     </Html>
-  )
+  );
 }
